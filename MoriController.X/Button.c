ba6a3569a584c9ee2d64d89faed09e7c649008ed
chbelke <christophbelke@icloud.com>
2020-03-11 17:15:12 +0100

    Update Button.c

--- MoriController.X/Button.c
+++ MoriController.X/Button.c
@@ -7,64 +7,30 @@
 volatile int k = 0;
 
 void Button_Eval() {
-    k = k + 1;
+    
+//    k = k + 1;
 //    switch (k) {
 //        case 1:
-//            MotRot_OUT(0, 1024);
-//            MotRot_OUT(1, 512);
-//            MotRot_OUT(2, 0);
+//            Coms_ESP_Drive(255, -127, 0, 1);
 //            break;
 //        case 2:
-//            MotRot_OUT(0, -1024);
-//            MotRot_OUT(1, -512);
-//            MotRot_OUT(2, 0);
+//            Coms_ESP_Drive(255, 0, 0, 1);
 //            break;
 //        case 3:
-//            MotRot_OUT(0, 512);
-//            MotRot_OUT(1, 256);
-//            MotRot_OUT(2, 0);
+//            Coms_ESP_Drive(255, 127, 0, 1);
+//            break;
+//        case 4:
+//            Coms_ESP_Drive(0, 0, 0, 1);
+//            Flg_EdgeDemo = true;
 //            break;
 //        default:
-//            MotRot_OUT(0, 0);
-//            MotRot_OUT(1, 0);
-//            MotRot_OUT(2, 0);
+//            Coms_ESP_Drive(0, 0, 0, 1);
+//            Flg_EdgeDemo = false;
 //            k = 0;
 //            break;
 //    }
-
-    switch (k) {
-        case 1:
-            Coms_ESP_Drive(255, -127, 0, 1);
-            break;
-        case 2:
-            Coms_ESP_Drive(255, -96, 0, 1);
-            break;
-        case 3:
-            Coms_ESP_Drive(255, -64, 0, 1);
-            break;
-        case 4:
-            Coms_ESP_Drive(255, -32, 0, 1);
-            break;
-        case 5:
-            Coms_ESP_Drive(255, 0, 0, 1);
-            break;
-        case 6:
-            Coms_ESP_Drive(255, 32, 0, 1);
-            break;
-        case 7:
-            Coms_ESP_Drive(255, 64, 0, 1);
-            break;
-        case 8:
-            Coms_ESP_Drive(255, 96, 0, 1);
-            break;
-        case 9:
-            Coms_ESP_Drive(255, 127, 0, 1);
-            break;
-        default:
-            Coms_ESP_Drive(0, 0, 0, 1);
-            k = 0;
-            break;
-    }
+    
+    SMA_On(0);
 }
 
 int Button_ReturnState() {
