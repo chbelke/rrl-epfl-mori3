86fae30c986d33bc48ef5ad1bf63b9201dc9b848
kholdcro <kevin.holdcroft@epfl.ch>
2020-10-11 16:26:16 +0200

    fixed bug where keys were in the wrong order

--- External/runMqtt/wirelessHost.py
+++ External/runMqtt/wirelessHost.py
@@ -118,7 +118,7 @@ class WirelessHost(threading.Thread):
     def publishLocal(self, msg, addr):
         try:
             if self.macDict[self.idDict[addr]][0] == "Lost":
-                print(colored("Unable to publish to " + names.nameToIds[addr], 'red'))
+                print(colored("Unable to publish to " + names.idsToName[addr], 'red'))
         except KeyError:
             pass
         if addr in self.noWifiDict:
@@ -133,7 +133,7 @@ class WirelessHost(threading.Thread):
         if addr not in self.noWifiDict:
             self.updateNoWifiDict(addr)
         if self.noWifiDict[addr][2] == False:
-            print(colored("ERROR: no path to hub for " + names.nameToIds[addr], 'red'))
+            print(colored("ERROR: no path to hub for " + names.idsToName[addr], 'red'))
             return
         msg = self.buildRelayMessage(addr, msg)
         host = self.noWifiDict[addr][0]
@@ -169,7 +169,7 @@ class WirelessHost(threading.Thread):
         self.coTimeDict[mac] = time.time()
         self.setCoTimeDict(self.coTimeDict)
         if not (self.macDict.get(mac) is None) and not (mac in self.macOrder): #Check if the ESP has been referenced and offline
-            print(colored(names.nameToIds[espNum] + " connected", "green"))
+            print(colored(names.idsToName[espNum] + " connected", "green"))
             self.macOrder.append(mac)
             self.setMacOrder(self.macOrder)
         return        
@@ -301,11 +301,11 @@ class WirelessHost(threading.Thread):
 
 
     def espUDP(self, espNum):
-        print(colored("UDP enabled for: " + names.nameToIds[espNum], 'green'))
+        print(colored("UDP enabled for: " + names.idsToName[espNum], 'green'))
         self.macDict.get(self.idDict[espNum])[0] = "UDP"
 
     def espLost(self, espNum):
-        print(colored("ESP " + names.nameToIds[espNum] + " lost", "red"))
+        print(colored("ESP " + names.idsToName[espNum] + " lost", "red"))
         self.macDict.get(self.idDict[espNum])[0] = "Lost"
         if espNum in self.noWifiDict:
             del self.noWifiDict[espNum]        
@@ -319,7 +319,7 @@ class WirelessHost(threading.Thread):
     def updateNoWifiDict(self, espNum):
         path, edge_path = self.shortestPath(espNum)
         if path == []:
-            print(colored("Error " + names.nameToIds[espNum] + " has no path to the hub", 'red'))
+            print(colored("Error " + names.idsToName[espNum] + " has no path to the hub", 'red'))
             self.macDict.get(self.idDict[espNum])[0] = "Lost"
         else:
             target_hub = path[0]
