67fa2c463c47393267abec7249e0e98d1fb30232
kholdcro <kevin.holdcroft@epfl.ch>
2021-01-27 10:41:25 +0100

    Added 8 new modules

--- External/Settings/names.py
+++ External/Settings/names.py
@@ -6,7 +6,15 @@ idsToName = {
 	"00D00BD5": "Carl",
 	"000AC175": "Darlene",
 	"000AC170": "Elaine",
-	"000AC173": "Farnsworth"
+	"000AC173": "Farnsworth",
+	"00D00BD2": "Gina",
+	"00D00BD3": "Hiro",
+	"00D00BD1": "Itchy",
+	"00D00BD0": "Jerry",
+	"00D00BCE": "Karen",
+	"00D00BCF": "Lucille",
+	"00D00BCD": "Maurice",
+	"00D00BCA": "Nancy",	
 }
 
 nameToIds = {
@@ -17,18 +25,34 @@ nameToIds = {
 	"Carl": "00D00BD5",
 	"Darlene": "000AC175",
 	"Elaine": "000AC170",
-	"Farnsworth": "000AC173"
+	"Farnsworth": "000AC173",
+	"Gina":"00D00BD2",
+	"Hiro":"00D00BD3",
+	"Itchy":"00D00BD1",
+	"Jerry":"00D00BD0",
+	"Karen":"00D00BCE",
+	"Lucille":"00D00BCF",
+	"Maurice":"00D00BCD",
+	"Nancy":"00D00BCA",
 }
 
 letterToIds = {
-	"H": "00056375",
-	"H": "0005659D",
+	"Z": "00056375",
+	"Z": "0005659D",
 	"A": "000AC171",
 	"B": "000AC172",
 	"C": "00D00BD5",
 	"D": "000AC175",
 	"E": "000AC170",
-	"F": "000AC173"
+	"F": "000AC173",
+	"G": "00D00BD2",
+	"H": "00D00BD3",
+	"I": "00D00BD1",
+	"J": "00D00BD0",
+	"K": "00D00BCE",
+	"L": "00D00BCF",
+	"M": "00D00BCD",
+	"N": "00D00BCA",	
 }
 
 
